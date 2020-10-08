3edd5726b5bbc87cd211e290a82ab7d4366d829d
khititach <khititach58@email.nu.ac.th>
2020-10-09 00:21:39 +0700

    Updates

---
+++ precache-manifest.658093696e2c6d1f9e15e17b473d1719.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab879a0b679033d99636380c4843813",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "782f0aa2999e6dff5013",
+    "url": "/portfolio/static/css/main.f0a7da93.chunk.css"
+  },
+  {
+    "revision": "e93f2704af563ee85ea5",
+    "url": "/portfolio/static/js/2.4fa7ff49.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.4fa7ff49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "782f0aa2999e6dff5013",
+    "url": "/portfolio/static/js/main.3406a326.chunk.js"
+  },
+  {
+    "revision": "5f87c37f8e4333840438",
+    "url": "/portfolio/static/js/runtime-main.dfc2f794.js"
+  },
+  {
+    "revision": "3a745d54a39ff97dca08c727692cb4e2",
+    "url": "/portfolio/static/media/800px-Naresuanuniversitylogo.3a745d54.png"
+  },
+  {
+    "revision": "2fcf9a37d8b6380c82575119c57f3e0f",
+    "url": "/portfolio/static/media/TPV Logo.2fcf9a37.webp"
+  },
+  {
+    "revision": "58925683d6fa9cbb64a0d0e6ba2d7866",
+    "url": "/portfolio/static/media/admin_home_page.58925683.jpg"
+  },
+  {
+    "revision": "06a4893f4071298d01c05fe351163856",
+    "url": "/portfolio/static/media/af.06a4893f.jpg"
+  },
+  {
+    "revision": "f6217c4c8133170367b70eb6bf70e9ac",
+    "url": "/portfolio/static/media/af2.f6217c4c.jpg"
+  },
+  {
+    "revision": "1ea5d2f87898d0e461798006e4523001",
+    "url": "/portfolio/static/media/highSchool.1ea5d2f8.png"
+  },
+  {
+    "revision": "75198a7c38f382d61801c2a397582f32",
+    "url": "/portfolio/static/media/home_page_new.75198a7c.jpg"
+  },
+  {
+    "revision": "869be2189c6c958fe9988e578493b93c",
+    "url": "/portfolio/static/media/student_home_page.869be218.jpg"
+  }
+]);
